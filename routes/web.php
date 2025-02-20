bc35a489a1cd0551184970bfda0c9211f67b2d0f
bwire40 <emmanuelbwire80@gmail.com>
2025-02-20 16:25:56 +0300

    designed navbar component

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -17,4 +15,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,11 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors:{
+                primary:"#fad02c",
+                secondary:"#000000",
+                accent:"#fbfbf9",
+            },
         },
     },
 
