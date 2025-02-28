eac9c2fa3a3f05f0bb097dfa080c306f87b232f0
bwire40 <emmanuelbwire80@gmail.com>
2025-02-28 15:07:09 +0300

    styiling

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+    css: {
+        postcss: {
+            plugins: [
+                require("tailwindcss"), // Make sure Tailwind is processed
+                require("autoprefixer"), // Autoprefixer for cross-browser support
+            ],
+        },
+    },
 });
